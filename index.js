c5f4d8ee9fae6ddf9eb13eac537af9d8c3c55823
Anusha Konatala <anu.konatala@gmail.com>
2018-09-04 22:37:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,7 @@ function driversByName(drivers){
 }
 
 function totalRevenue(drivers){
-  
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
 }
