a30dcf26aa42d298f6378dc9294b5021d545d55d
Anusha Konatala <anu.konatala@gmail.com>
2018-09-04 22:16:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ function driversByRevenue(drivers){
   });
 }
 
+function driversByName(drivers){
+  const sortDrivers= drivers.slice();
+  return sortDrivers.sort(function(a, b) {
+    return a.name - b.name;
+  });
+}
